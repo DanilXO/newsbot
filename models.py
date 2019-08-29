4b6c53047131ae33b1df93e52fd72d3c477cb219
Frolov Daniil <d.frolov@directline.digital>
2019-08-29 17:48:18 +0400

    + db

--- models.py
+++ models.py
@@ -1,3 +1,79 @@
-from sqlalchemy import create_engine
+import requests
+from bs4 import BeautifulSoup
+from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+Base = declarative_base()
+engine = create_engine('sqlite:///test', echo=True)
+
+
+class VkUser(Base):
+    __tablename__ = 'vk_users'
+    vk_user_id = Column('vk_user_id', Integer, primary_key=True, unique=True)
+    fullname = Column('fullname', String)
+
+    def __init__(self, vk_user_id):
+        self.vk_user_id = vk_user_id
+        self.fullname = self._get_user_name_from_vk_id(vk_user_id)
+
+    def _get_user_name_from_vk_id(self, user_id):
+        request = requests.get("https://vk.com/id" + str(user_id))
+        bs = BeautifulSoup(request.text, "html.parser")
+
+        user_name = self._clean_all_tag_from_str(bs.findAll("title")[0])
+
+        return user_name.split()[0]
+
+    @staticmethod
+    def _clean_all_tag_from_str(string_line):
+        """
+        Очистка строки stringLine от тэгов и их содержимых
+        :param string_line: Очищаемая строка
+        :return: очищенная строка
+        """
+        result = ""
+        not_skip = True
+        for i in list(string_line):
+            if not_skip:
+                if i == "<":
+                    not_skip = False
+                else:
+                    result += i
+            else:
+                if i == ">":
+                    not_skip = True
+        return result
+
+    def __repr__(self):
+        return "<VkUser('%s','%s')>" % (self.vk_user_id, self.fullname)
+
+
+class TelegramUser(Base):
+    __tablename__ = 'telegram_user'
+    telegram_id = Column('telegram_id', Integer, primary_key=True, unique=True)
+    fullname = Column('fullname', String)
+
+    def __init__(self, name, fullname, password):
+        self.name = name
+        self.fullname = fullname
+
+    def __repr__(self):
+        return "<TelegramUser('%s','%s')>" % (self.telegram_id, self.fullname)
+
+
+class Keyword(Base):
+    __tablename__ = 'keywords'
+    id = Column('id', Integer, primary_key=True, autoincrement=True, unique=True)
+    vk_user_id = Column('vk_user_id', Integer, ForeignKey("vk_users.vk_user_id"), nullable=True),
+    name = Column('name', String)
+
+    def __init__(self, name, fullname, password):
+        self.name = name
+
+    def __repr__(self):
+        return "<Keyword('%s')>" % (self.name)
+
+
+if __name__ == '__main__':
+    # Создание таблицы
+    Base.metadata.create_all(engine)
--- vk_bot.py
+++ vk_bot.py
@@ -1,10 +1,15 @@
 import random
+import re
 import sys
 
 import requests
 import vk_api
 from bs4 import BeautifulSoup
+from sqlalchemy.orm import sessionmaker
 from vk_api.longpoll import VkLongPoll, VkEventType
+
+from models import engine, VkUser
+from newsparser import NewsParser
 
 
 class VkBot:
@@ -14,20 +19,21 @@ class VkBot:
         self._SESSION = vk_api.VkApi(token=self._TOKEN)
         with open('keyboard.json', 'r') as kb:
             self.keyboard = kb.read()
-        self._COMMANDS = ["ПОДПИСАТЬСЯ", "ОТПИСАТЬСЯ", "МОИ_ИНТЕРЕСЫ", "ЗАДАТЬ_НОВЫЕ_ИНТЕРЕСЫ", "ДОБАВИТЬ_ИНТЕРЕС"]
         self._ACTIONS = {"ПОДПИСАТЬСЯ": self.subscribe,
                          "ОТПИСАТЬСЯ": self.unsubscribe,
+                         "ПОПУЛЯРНОЕ": self.get_popular_news,
                          "МОИ_ИНТЕРЕСЫ": self.get_user_interests,
                          "ЗАДАТЬ_НОВЫЕ_ИНТЕРЕСЫ": self.set_user_interests,
                          "ДОБАВИТЬ_ИНТЕРЕС": self.add_user_interest,
                          }
-        self.USERS = []
-        self.USERS_INTERESTS = {}
+        self._COMANDS = list(self._ACTIONS.keys())
+        self.db_session = sessionmaker(bind=engine)()
         self.api = None
+        self.news_parser = NewsParser(lang='ru')
 
     def subscribe(self, user_id):
         """ Подписаться на новости """
-        if user_id in self.USERS:
+        if self.db_session.query(VkUser).filter_by(vk_user_id=user_id).first():
             self.api.messages.send(
                 user_id=user_id,
                 random_id=random.randint(0, sys.maxsize),
@@ -35,7 +41,9 @@ class VkBot:
                 keyboard=self.keyboard
             )
         else:
-            self.USERS.append(user_id)
+            new_user = VkUser(user_id)
+            self.db_session.add(new_user)
+            self.db_session.commit()
             self.api.messages.send(
                 user_id=user_id,
                 random_id=random.randint(0, sys.maxsize),
@@ -46,16 +54,10 @@ class VkBot:
 
     def unsubscribe(self, user_id):
         """ Отписаться от новостей """
-        if user_id not in self.USERS:
-            self.api.messages.send(
-                user_id=user_id,
-                random_id=random.randint(0, sys.maxsize),
-                message="Вы ещё не подписались на новостную рассылку.",
-                keyboard=self.keyboard
-            )
-        else:
-            self.USERS.remove(user_id)
-            self.USERS_INTERESTS.pop(user_id, None)
+        user = self.db_session.query(VkUser).filter_by(vk_user_id=user_id).first()
+        if user:
+            self.db_session.delete(user)
+            self.db_session.commit()
             self.api.messages.send(
                 user_id=user_id,
                 random_id=random.randint(0, sys.maxsize),
@@ -63,13 +65,21 @@ class VkBot:
                 keyboard=self.keyboard
             )
 
-    def get_user_interests(self, user_id):
-        """ Получить интересы пользователя """
-        if self.USERS_INTERESTS.get(user_id) is not None:
+        else:
             self.api.messages.send(
                 user_id=user_id,
                 random_id=random.randint(0, sys.maxsize),
-                message=f"Ваши интересы: {self.USERS_INTERESTS.get(user_id)}",
+                message="Вы ещё не подписались на новостную рассылку.",
+                keyboard=self.keyboard
+            )
+
+    def get_user_interests(self, user_id):
+        """ Получить интересы пользователя """
+        if self.USERS_INTERESTS.get(user_id, None) is not None:
+            self.api.messages.send(
+                user_id=user_id,
+                random_id=random.randint(0, sys.maxsize),
+                message="Ваши интересы: {}".format(", ".join(self.USERS_INTERESTS.get(user_id))),
                 keyboard=self.keyboard
             )
         else:
@@ -82,14 +92,52 @@ class VkBot:
 
     def set_user_interests(self, user_id, interests):
         """ Задает новые интересы пользователя """
-        self.USERS_INTERESTS[user_id] = interests
+        self.USERS_INTERESTS[user_id] = re.findall(r"[\w']+", interests)
+        self.api.messages.send(
+            user_id=user_id,
+            random_id=random.randint(0, sys.maxsize),
+            message="Вы успешно добавили новые интересы! "
+                    "Теперь вы будете получать новостную подборку согласно вашим предпочтениям.",
+            keyboard=self.keyboard
+        )
 
     def add_user_interest(self, user_id, interests):
         """ Добавляет новый интерес пользователя """
-        if self.USERS_INTERESTS[user_id]:
-            self.USERS_INTERESTS[user_id] = self.USERS_INTERESTS[user_id] + ", " + interests
+        if self.USERS_INTERESTS.get(user_id, None):
+            self.USERS_INTERESTS[user_id].append(re.findall(r"[\w']+", interests))
         else:
-            self.USERS_INTERESTS[user_id] = interests
+            self.USERS_INTERESTS[user_id] = re.findall(r"[\w']+", interests)
+        self.api.messages.send(
+            user_id=user_id,
+            random_id=random.randint(0, sys.maxsize),
+            message="Вы успешно добавили новые интересы! "
+                    "Теперь вы будете получать новостную подборку согласно вашим предпочтениям.",
+            keyboard=self.keyboard
+        )
+
+    def get_popular_news(self, user_id):
+        # if self.USERS_INTERESTS.get(user_id):
+        #     news_list = self.news_parser.get_news(self.USERS_INTERESTS.get(user_id))[:2]
+        # else:
+        #     news_list = self.news_parser.get_news([])[:2]
+        news_list = self.news_parser.get_news(["Apple", "Android"])[:3]
+        if not news_list:
+            self.api.messages.send(
+                user_id=user_id,
+                random_id=random.randint(0, sys.maxsize),
+                message="Ничего не найдено. Будем держать вас в курсе...",
+                keyboard=self.keyboard
+            )
+        else:
+            row_spliter = "".join("_" for i in range(65))
+            for new in news_list:
+                self.api.messages.send(
+                    user_id=user_id,
+                    random_id=random.randint(0, sys.maxsize),
+                    message="Новость:\n_________\n❗{}❗\n\n{}\nПодробне:{}\n\n\n".format(new["title"], new["description"],
+                                                                                 new["link"]),
+                    keyboard=self.keyboard
+                )
 
     def _connect(self):
         try:
@@ -102,13 +150,20 @@ class VkBot:
         for event in longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW:
                 if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-                    command = event.text.upper().replace(' ', '_')
+                    comand = event.text.upper().replace(' ', '_')
 
-                    if command in self._COMMANDS:
-                        if command in self._COMMANDS[:3]:
-                            self._ACTIONS[command](event.user_id)
-                        elif command in self._COMMANDS[3:]:
-                            previous_command = command
+                    if comand in self._COMANDS:
+                        if comand in self._COMANDS[:4]:
+                            self._ACTIONS[comand](event.user_id)
+                        elif comand in self._COMANDS[4:]:
+                            previous_command = comand
+                            self.api.messages.send(
+                                user_id=event.user_id,
+                                random_id=random.randint(0, sys.maxsize),
+                                message="Перечислите интересующие вас тематики. "
+                                        "Например: Apple, Android, Политика",
+                                keyboard=self.keyboard
+                            )
                     elif previous_command is not None:
                         self._ACTIONS[previous_command](event.user_id, event.text)
                         previous_command = None
@@ -128,37 +183,27 @@ class VkBot:
         self._eventloop(longpoll)
 
 
-class VkUser:
-    """ Класс, представляющий отдельного юзера, подписавшегося на наши обновления """
-
-    def __init__(self, user_id):
-        self.USER_ID = user_id
-        self._USERNAME = self._get_user_name_from_vk_id(user_id)
-
-    def _get_user_name_from_vk_id(self, user_id):
-        request = requests.get("https://vk.com/id" + str(user_id))
-        bs = BeautifulSoup(request.text, "html.parser")
-
-        user_name = self._clean_all_tag_from_str(bs.findAll("title")[0])
-
-        return user_name.split()[0]
-
-    @staticmethod
-    def _clean_all_tag_from_str(string_line):
-        """
-        Очистка строки stringLine от тэгов и их содержимых
-        :param string_line: Очищаемая строка
-        :return: очищенная строка
-        """
-        result = ""
-        not_skip = True
-        for i in list(string_line):
-            if not_skip:
-                if i == "<":
-                    not_skip = False
-                else:
-                    result += i
-            else:
-                if i == ">":
-                    not_skip = True
-        return result
+# class VkUser:
+#     """ Класс, представляющий отдельного юзера, подписавшегося на наши обновления """
+#
+#     def __init__(self, user_id):
+#         self.USER_ID = user_id
+#         self._USERNAME = self._get_user_name_from_vk_id(user_id)
+#
+#     def _get_user_name_from_vk_id(self, user_id):
+#         request = requests.get("https://vk.com/id" + str(user_id))
+#         bs = BeautifulSoup(request.text, "html.parser")
+#
+#         user_name = self._clean_all_tag_from_str(bs.findAll("title")[0])
+#
+#         return user_name.split()[0]
+#
+#     @staticmethod
+#     def _clean_all_tag_from_str(string_line):
+#         """
+#         Очистка строки stringLine от тэгов и их содержимых
+#         :param string_line: Очищаемая строка
+#         :return: очищенная строка
+#         """
+#         soup = BeautifulSoup(string_line)
+#         return soup.get_text()
