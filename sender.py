4c2124c70d16df14e428cdbc20b6909d05ae8e65
Frolov <byka72@gmail.com>
2019-11-15 19:06:14 +0400

    sender fix

--- sender.py
+++ sender.py
@@ -1,6 +1,5 @@
 import time
 import logging
-from sqlalchemy.exc import ProgrammingError
 from sqlalchemy.orm import sessionmaker
 
 from models import engine, VkUser, Keyword
@@ -11,14 +10,22 @@ vk_bot = VkBot()
 vk_bot.gen_api()
 
 log = logging.getLogger("botlog")
-while True:
-    try:
-        keywords = [interest.name for interest in db_session.query(Keyword).all()]
-        if keywords:
-            for keyword in keywords:
-                users_with_that_keyword = db_session.query(VkUser.vk_user_id).filter(VkUser.keyword.any(name=keyword)).all()
-                vk_bot.send_news_for_many_users([_.vk_user_id for _ in users_with_that_keyword], keyword)
-                time.sleep(5)
-        time.sleep(5)
-    except Exception as ex:
-        log.error(ex)
+
+
+def run_mailing():
+    while True:
+        try:
+            keywords = [interest.name for interest in db_session.query(Keyword).all()]
+            if keywords:
+                for keyword in keywords:
+                    users_with_that_keyword = db_session.query(VkUser.vk_user_id).filter(VkUser.keyword.any(name=keyword)).all()
+                    vk_bot.send_news_for_many_users([_.vk_user_id for _ in users_with_that_keyword], keyword)
+                    time.sleep(5)
+            time.sleep(5)
+        except Exception as ex:
+            log.error(ex)
+            run_mailing()
+
+
+run_mailing()
+
