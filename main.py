5e00039b21c68e8b5ba5d57b89b09848bc1033ff
Frolov Daniil <d.frolov@directline.digital>
2019-11-15 14:24:18 +0400

    fix imports

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sys
 import os.path
-sys.path.append('..')
-from vk_bot import VkBot
-import subprocess
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 DEBUG = False
 if __name__ == '__main__':
+    from vk_bot import VkBot
+    import subprocess
     bot = VkBot()
     sender = subprocess.Popen([sys.executable, "sender.py"])
     bot.run()
